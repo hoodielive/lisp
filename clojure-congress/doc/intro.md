a82a71e52f643740e3d6f0abc0690ea76776da37
hoodielive <hoodielive@gmail.com>
2018-11-17 23:26:20 -0500

    clojure congress

---
+++ clojure-congress/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-congress
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure-congress/src/clojure_congress/core.clj
@@ -0,0 +1,7 @@
+(ns clojure-congress.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
